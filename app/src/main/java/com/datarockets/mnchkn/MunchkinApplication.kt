16a41859cbbb56e2b0c72c6a3e7974ec6bcf62ce
Dzmitry Chyrta <dima@chyrta.com>
2017-01-23 16:34:39 +0300

    Implemented Fabric Crashlytics to catch unexpected issues

--- app/src/main/java/com/datarockets/mnchkn/MunchkinApplication.kt
+++ app/src/main/java/com/datarockets/mnchkn/MunchkinApplication.kt
@@ -2,28 +2,36 @@ package com.datarockets.mnchkn
 
 import android.app.Application
 import android.content.Context
+import com.crashlytics.android.Crashlytics
 
 import com.datarockets.mnchkn.injection.components.ApplicationComponent
 import com.datarockets.mnchkn.injection.components.DaggerApplicationComponent
 import com.datarockets.mnchkn.injection.modules.ApplicationModule
 import com.mixpanel.android.mpmetrics.MixpanelAPI
+import io.fabric.sdk.android.Fabric
 
 import timber.log.Timber
+import javax.inject.Inject
 
 class MunchkinApplication : Application() {
 
-    lateinit var component: ApplicationComponent
-    var mMixpanel: MixpanelAPI? = null
+    lateinit var mApplicationComponent: ApplicationComponent
+    private var mMixpanel: MixpanelAPI? = null
+
+    @Inject lateinit var mCrashlytics: Crashlytics
 
     override fun onCreate() {
         super.onCreate()
 
         if (BuildConfig.DEBUG) Timber.plant(Timber.DebugTree())
 
-        component = DaggerApplicationComponent.builder()
+        mApplicationComponent = DaggerApplicationComponent.builder()
                 .applicationModule(ApplicationModule(this))
                 .build()
-        component.inject(this)
+        mApplicationComponent.inject(this)
+
+        Fabric.with(this, mCrashlytics)
+
     }
 
     val mixpanel: MixpanelAPI
@@ -35,7 +43,6 @@ class MunchkinApplication : Application() {
         }
 
     companion object {
-
         operator fun get(context: Context): MunchkinApplication {
             return context.applicationContext as MunchkinApplication
         }
--- app/src/main/java/com/datarockets/mnchkn/injection/modules/ApplicationModule.kt
+++ app/src/main/java/com/datarockets/mnchkn/injection/modules/ApplicationModule.kt
@@ -2,8 +2,12 @@ package com.datarockets.mnchkn.injection.modules
 
 import android.app.Application
 import android.content.Context
+import com.crashlytics.android.Crashlytics
+import com.crashlytics.android.core.CrashlyticsCore
+import com.datarockets.mnchkn.BuildConfig
 
 import com.datarockets.mnchkn.injection.ApplicationContext
+import com.mixpanel.android.mpmetrics.MixpanelAPI
 
 import dagger.Module
 import dagger.Provides
@@ -20,6 +24,20 @@ class ApplicationModule(protected val mApplication: Application) {
     @ApplicationContext
     fun providesContext(): Context {
         return mApplication
+    }
+
+    @Provides
+    fun providesCrashlyticsCore(): CrashlyticsCore {
+        return CrashlyticsCore.Builder()
+                .disabled(BuildConfig.DEBUG)
+                .build()
+    }
+
+    @Provides
+    fun providesCrashlytics(crashlyticsCore: CrashlyticsCore): Crashlytics {
+        return Crashlytics.Builder()
+                .core(crashlyticsCore)
+                .build()
     }
 
 }
