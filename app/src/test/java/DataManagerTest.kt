59903a84a67899011995dcd05be79722d305e198
Dzmitry Chyrta <dima@chyrta.com>
2017-02-01 16:45:51 +0300

    Mocked up tests for players list presenter and data manager

--- app/src/test/java/DataManagerTest.kt
+++ app/src/test/java/DataManagerTest.kt
@@ -1,5 +1,24 @@
-/**
- * Created by Blackmagic on 31.01.17.
- */
+import com.datarockets.mnchkn.data.DataManager
+import com.datarockets.mnchkn.data.local.DatabaseHelper
+import com.datarockets.mnchkn.data.local.PreferencesHelper
+import org.junit.Before
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.mockito.Mock
+import org.mockito.Mockito
+import org.mockito.Mockito.verify
+import org.mockito.runners.MockitoJUnitRunner
+
+@RunWith(MockitoJUnitRunner::class)
 class DataManagerTest {
+
+    @Mock lateinit var mMockPreferencesHelper: PreferencesHelper
+    @Mock lateinit var mMockDatabaseHelper: DatabaseHelper
+    private lateinit var mDataManager: DataManager
+
+    @Before
+    fun setUp() {
+        mDataManager = DataManager(mMockDatabaseHelper, mMockPreferencesHelper)
+    }
+
 }
---
+++ app/src/test/java/PlayersListPresenterTest.kt
@@ -0,0 +1,15 @@
+import org.junit.Before
+import org.junit.runner.RunWith
+import org.mockito.runners.MockitoJUnitRunner
+
+@RunWith(MockitoJUnitRunner::class)
+class PlayersListPresenterTest {
+
+
+    @Before
+    fun setUp() {
+        throw UnsupportedOperationException("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+
+}
